Fix sine term calculation and increase sample size

7.py
@@ -12,12 +12,12 @@ def f_samtals(x):
 def nakvaem_lausn_cc_sin(x):
     f = f_kraftur()
     y_const = (f / (24 * E * I)) * x**2 * (x - L)**2
-    y_sin = -(p * g / (E * I)) * (L**4 / np.pi**4) * np.sin(np.pi * x / L)
+    y_sin = -(p * g / (E * I)) * (L**3 / np.pi**4) * np.sin(np.pi * x / L)
     y_poly = (p * g / (E * I)) * ((-L / np.pi**3) * x**2 + (L**2 / np.pi**3) * x)
     return y_const + y_sin + y_poly
 
 
-n = 20
+n = 2560
 x, y = leysa_fylki_breytilegt(n, f_samtals, boundary="fast")
 
 xf = np.linspace(0, L, 400)